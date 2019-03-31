a7ac91a937ce5fe18e56e555c7cc534f9f6d2fd3
piokowalski <piokowalski@onet.pl>
2019-03-31 15:50:37 +0200

    Class statistics created

--- src/bikes/app/statistics/Statistics.java
+++ src/bikes/app/statistics/Statistics.java
@@ -1,4 +1,17 @@
 package bikes.app.statistics;
 
 public class Statistics {
+    //statistics of count of bike stations in specific country
+    //and cities
+
+    public String country;
+    public String city;
+
+    public Integer bikeStations;
+
+
+
+
+
+
 }
