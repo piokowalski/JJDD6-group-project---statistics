938e33ddd9799d1ea078a2ff02937a5ced96bdb2
piokowalski <piokowalski@onet.pl>
2019-03-31 16:05:31 +0200

    Methods for country and cities added - comment

--- src/bikes/app/statistics/Statistics.java
+++ src/bikes/app/statistics/Statistics.java
@@ -4,13 +4,17 @@ public class Statistics {
     //statistics of count of bike stations in specific country
     //and cities
 
+    //methods should take informations from xml file
+    //countries should be categorized with cities
+
 
     public void countryStats(Integer bikeStations, String country) {
         System.out.println("In " + country + " there is: " + bikeStations + " bike stations.");
     }
-        public void cityStats (Integer bikeStations, String city){
-            System.out.println("In " + city+ " there is: " + bikeStations + " bike stations.");
-        }
 
-
+    public void cityStats(Integer bikeStations, String city) {
+        System.out.println("In " + city + " there is: " + bikeStations + " bike stations.");
     }
+
+
+}
