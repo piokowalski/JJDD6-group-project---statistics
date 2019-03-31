dbd9ec0963d611d68e0bc1688b7bb7c4ae4d858a
piokowalski <piokowalski@onet.pl>
2019-03-31 15:32:26 +0200

    Initial commit

---
+++ src/bikes/app/App.java
@@ -0,0 +1,8 @@
+package bikes.app;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("Rowerek");
+    }
+}
---
+++ src/bikes/app/statistics/Statistics.java
@@ -0,0 +1,4 @@
+package bikes.app.statistics;
+
+public class Statistics {
+}
