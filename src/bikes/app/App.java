a4227af27cad8479f9b1b7769a359c3ecde85290
piokowalski <piokowalski@onet.pl>
2019-04-02 09:40:54 +0200

    Bikers ASCII picture added

--- src/bikes/app/App.java
+++ src/bikes/app/App.java
@@ -12,5 +12,9 @@ public class App {
         statistics.cityStats(1,"Gdansk");
 
         statistics.countryStats(320,"Germany");
+
+        System.out.println(" -------- __@      __@       __@       __@     __~@\n" +
+                " ----- _`\\<,_    _`\\<,_    _`\\<,_     _`\\<,_    _`\\<,_\n" +
+                " ---- (*)/ (*)  (*)/ (*)  (*)/ (*)  (*)/ (*)  (*)/ (*)\n");
     }
 }
